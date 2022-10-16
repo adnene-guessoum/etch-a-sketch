685ff9ae505c041854fa0bf14bc95112287859be
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-16 11:47:39 +0200

    Add background countainer div and first grid element with black border

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const gridDiv = document.querySelector("#grid-container")
 
 const firstDiv = document.createElement("div")
-
+firstDiv.classList.toggle('grid-squares')
 
 gridDiv.appendChild(firstDiv)
 
