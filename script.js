c76b5bc7ab3fd2f30a284dc15c93e38d53d0b5e9
Adnene_wsl <adnen.guessoum@gmail.com>
2022-10-17 09:36:27 +0200

    Remove previous children when rebuilding canvas with user input

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
 const gridDiv = document.querySelector("#grid-container")
 
-for (let i = 0 ; i<=255; i++) {
+function createGrid(size) {
 
-const gridItems = document.createElement("div")
-gridItems.classList.toggle('grid-squares')
-gridDiv.appendChild(gridItems)
+	// clear canvas
+	while ( gridDiv.firstChild ) gridDiv.removeChild( gridDiv.firstChild );
+
+	//check prompt
+	if (size > 100) {
+		alert("No more than 100 squares per line/column");
+		size = 16;
+	}
+
+	//set size from user input
+	gridDiv.style.setProperty('--grid-rows', size);
+    gridDiv.style.setProperty('--grid-cols', size);
+
+	//rebuild canvas with new size
+	for (let i = 0 ; i<=((size*size - 1)); i++) {
+		const gridItems = document.createElement("div")
+		gridItems.classList.toggle('grid-squares')
+		gridDiv.appendChild(gridItems)
+	}
 
 }
-
-const items = gridDiv.querySelectorAll("div")
 
 function switchClass(obj) {
 	/*
@@ -26,13 +40,23 @@ function resetClass(obj) {
 	obj.classList.remove('color-switch')
 }
 
-const coloredItem = items.forEach( item => item.addEventListener("mouseover", e =>  switchClass(e.target)));
+function interactionLogic(size) {
+	createGrid(size);
+	const items = gridDiv.querySelectorAll("div")
+	const coloredItem = items.forEach( item => item.addEventListener("mouseover", e =>  switchClass(e.target)));
 
-const clearBtn = document.getElementById("clear");
-clearBtn.addEventListener("click", e => items.forEach(resetClass)); 
+	const clearBtn = document.getElementById("clear");
+	clearBtn.addEventListener("click", e => items.forEach(resetClass)); 
+
+}
+
+interactionLogic(16)
 
 const sizeBtn = document.getElementById("size");
 sizeBtn.addEventListener("click", function(e) {
-	askPopUp = prompt("what size grid do you need ? (max 100x100)");
-
+	sizeGrid = prompt("what size grid do you need ? (max 100x100)", 16);
+	interactionLogic(sizeGrid)
 });
+
+
+
